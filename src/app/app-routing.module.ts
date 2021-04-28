5cb862bffec90597995da00c2bfbf3d5e5da26bd
Győrffy Péter <66072584+Exterior7@users.noreply.github.com>
2021-04-28 12:56:24 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+import { ProductsDetailComponent } from './component/products-detail/products-detail.component';
+import { ProductsComponent } from './component/products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:"home",
+    component: HomeComponent
+  },
+  {
+    path:"products",
+    component: ProductsComponent
+  },
+  {
+    path:"products/:id",
+    component: ProductsDetailComponent
+  },
+  {
+    path:"**",
+    component: HomeComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
